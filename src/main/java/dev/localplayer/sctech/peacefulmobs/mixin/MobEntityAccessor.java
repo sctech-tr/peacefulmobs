f82c9e570d104cee52b5acad2842cb2c0bb93ed8
sctech <gamerselimiko@gmail.com>
2026-05-10 16:43:13 +0300

    1.2.0 for 26.1.2

--- src/main/java/dev/localplayer/sctech/peacefulmobs/mixin/MobEntityAccessor.java
+++ src/main/java/dev/localplayer/sctech/peacefulmobs/mixin/MobEntityAccessor.java
@@ -1,12 +1,12 @@
 
-package dev.localplayer.sctech.peacefulmobs.mixins;
+package dev.localplayer.sctech.peacefulmobs.mixin;
 
-import net.minecraft.entity.mob.MobEntity;
-import net.minecraft.entity.ai.goal.GoalSelector;
+import net.minecraft.world.entity.Mob;
+import net.minecraft.world.entity.ai.goal.GoalSelector;
 import org.spongepowered.asm.mixin.Mixin;
 import org.spongepowered.asm.mixin.gen.Accessor;
 
-@Mixin(MobEntity.class)
+@Mixin(Mob.class)
 public interface MobEntityAccessor {
     @Accessor("targetSelector")
     GoalSelector getTargetSelector();
